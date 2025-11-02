772153b40ddda88fa7a5c308e6e13dae0d700110
Hugo <hugo.vrana@togolive.de>
2025-11-02 18:16:36 +0100

    start

--- src/main/java/com/dashboard/oauth/DashboardOauthApplication.java
+++ src/main/java/com/dashboard/oauth/DashboardOauthApplication.java
@@ -1,4 +1,4 @@
-package com.dashboard.dashboard.oauth;
+package com.dashboard.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
