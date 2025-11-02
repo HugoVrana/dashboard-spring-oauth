49645d02249bcba643179972c482a02e085d6642
Hugo <hugo.vrana@togolive.de>
2025-11-02 17:58:21 +0100

    start

---
+++ src/main/java/com/dashboard/dashboard/oauth/DashboardOauthApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.dashboard.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dashboard/dashboard/oauth/DashboardOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.dashboard.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
