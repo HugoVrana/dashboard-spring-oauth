37e7aa622c0cde7c084652321e204059411e15f4
Hugo <hugo.vrana@togolive.de>
2025-11-02 18:50:17 +0100

    setup

--- src/main/java/com/dashboard/oauth/DashboardOauthApplication.java
+++ src/main/java/com/dashboard/oauth/DashboardOauthApplication.java
@@ -2,9 +2,17 @@ package com.dashboard.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DashboardOauthApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Dashboard is here!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DashboardOauthApplication.class, args);
