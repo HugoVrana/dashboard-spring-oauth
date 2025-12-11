a3c0813d1b91e073ff5e0a234e3d6a70faaee737
Hugo Vrana <hugo.vrana@swissmarketplace.group>
2025-12-11 13:58:31 +0100

    should work

--- src/main/java/com/dashboard/oauth/mapper/UserInfoMapper.java
+++ src/main/java/com/dashboard/oauth/mapper/UserInfoMapper.java
@@ -1,5 +1,6 @@
 package com.dashboard.oauth.mapper;
 
+import com.dashboard.oauth.dataTransferObject.role.RoleRead;
 import com.dashboard.oauth.dataTransferObject.user.UserInfoRead;
 import com.dashboard.oauth.mapper.interfaces.IUserInfoMapper;
 import com.dashboard.oauth.model.UserInfo;
@@ -12,6 +13,8 @@ public class UserInfoMapper implements IUserInfoMapper {
         UserInfoRead userInfoRead = new UserInfoRead();
         userInfoRead.setId(userInfo.getId().toHexString());
         userInfoRead.setEmail(userInfo.getEmail());
+        RoleRead[] roleReadList = new RoleRead[]{};
+        userInfoRead.setRoleReads(roleReadList);
         return userInfoRead;
     }
 }
