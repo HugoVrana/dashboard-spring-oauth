9e2b5e31948cc913189106cb6d5510784b258a34
HugoVrana <hugo.vrana@togolive.de>
2026-03-01 00:04:14 +0100

    fix for something

--- src/main/java/com/dashboard/oauth/controller/RootController.java
+++ src/main/java/com/dashboard/oauth/controller/RootController.java
@@ -1,14 +1,20 @@
 package com.dashboard.oauth.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@Hidden
 public class RootController {
 
     @GetMapping("/")
-    public RedirectView redirectToSwagger() {
-        return new RedirectView("/swagger-ui/index.html");
+    public ResponseEntity<Void> redirectToSwagger() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "/swagger-ui/index.html");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
