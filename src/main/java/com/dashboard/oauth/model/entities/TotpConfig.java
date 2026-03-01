0cd7ab2bf7fc2101971273398b2f590aa853ad17
HugoVrana <hugo.vrana@togolive.de>
2026-03-01 18:26:21 +0100

    linter fix

--- src/main/java/com/dashboard/oauth/model/entities/TotpConfig.java
+++ src/main/java/com/dashboard/oauth/model/entities/TotpConfig.java
@@ -1,11 +1,8 @@
 package com.dashboard.oauth.model.entities;
 
-import com.dashboard.common.model.Audit;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.Instant;
 
 @Data
 @NoArgsConstructor
