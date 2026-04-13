34ed09a62aa0b0fbe1b95d6f20b3651eb252f282
HugoVrana <hugo.vrana@togolive.de>
2026-04-13 16:16:58 +0200

    discovered issues in tests

--- src/main/java/com/dashboard/oauth/repository/IAuthorizationRequestRepository.java
+++ src/main/java/com/dashboard/oauth/repository/IAuthorizationRequestRepository.java
@@ -4,8 +4,9 @@ import com.dashboard.oauth.model.entities.oauth.AuthorizationRequest;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface IAuthorizationRequestRepository extends MongoRepository<AuthorizationRequest, ObjectId> {
-    Optional<AuthorizationRequest> findByIdAndUsedFalseAndAudit_DeletedAtIsNull(ObjectId id);
+    Optional<AuthorizationRequest> findBy_IdAndUsedTrueAndAudit_DeletedAtIsNull(ObjectId id);
 }
